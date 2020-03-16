0d34154c71381dcea080e45436d831ad162aadb9
Alexey <kalinichenkoae@gmail.com>
2020-03-16 09:12:40 +0300

    Add progress bar animation

---
+++ Assets/Scripts/Game/Scenes/Load/ProgressBar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressBar : BaseLogic
+{
+    private RectTransform mRectTransform;
+    private float mFillingSpeed;
+    
+    private float mWidthMin;
+    private float mWidthMax;
+    private float mWidth;
+    
+    void Start()
+    {
+        mRectTransform = GetComponent<RectTransform>();
+        mFillingSpeed = 1f;
+        
+        mWidthMin = 0f;
+        mWidthMax = 4.55f;
+        mWidth = mWidthMin;
+        
+        mRectTransform.localScale = new Vector2(mWidthMin, mRectTransform.localScale.y);
+    }
+
+    void Update()
+    {
+        mWidth = mRectTransform.localScale.x;
+
+        if (mWidth < mWidthMax)
+        {
+            mWidth += mFillingSpeed * Time.deltaTime;
+            
+            mRectTransform.localScale = new Vector2(mWidth, mRectTransform.localScale.y);
+        }
+    }
+}
