7fc3a2aeb4edc19ef2b0f0722a7831a41ca5c617
Alexey <kalinichenkoae@gmail.com>
2020-03-29 12:45:34 +0300

    Change loading logic

--- Assets/Scripts/Game/Scenes/Load/Loading.cs
+++ Assets/Scripts/Game/Scenes/Load/Loading.cs
@@ -9,6 +9,8 @@ public class Loading : BaseLogic
     {
         INITIAL_STATE,
         IN_PROGRESS,
+        TASK_FAILED,
+        INACTIVE,
         VERSION_CHECKED,
         MANAGERS_LOADED,
         GENERAL_LOADED,
@@ -18,20 +20,55 @@ public class Loading : BaseLogic
 
     private eLoadingStatus mStatus;
 
-    private TaskQueue mTaskQueue;
+    private Dictionary<eLoadingStatus, bool> mLoadingProgress;
 
     private ProgressBar mProgressBar;
-    
-    private void OnTerminated(string taskName)
+
+    private void OnLoadingTerminated()
     {
-        Debug.Log(taskName + " : failed");
+        string failedTask = "*";
+
+        foreach (var taskStatus in mLoadingProgress)
+        {
+            if (!taskStatus.Value)
+            {
+                switch (taskStatus.Key)
+                {
+                    case eLoadingStatus.VERSION_CHECKED:
+                        failedTask = "CheckGameVersion";
+                        break;
+
+                    case eLoadingStatus.MANAGERS_LOADED:
+                        failedTask = "CheckGameVersion";
+                        break;
+
+                    case eLoadingStatus.GENERAL_LOADED:
+                        failedTask = "CheckGameVersion";
+                        break;
+
+                    case eLoadingStatus.CONFIGS_LOADED:
+                        failedTask = "CheckGameVersion";
+                        break;
+
+                    case eLoadingStatus.PROFILE_LOADED:
+                        failedTask = "CheckGameVersion";
+                        break;
+                }
+            }
+        }
+        
+        Debug.Log(failedTask + " : failed");
+
+        mStatus = eLoadingStatus.INACTIVE;
     }
     
-    private void OnCompleted()
+    private void OnLoadingCompleted()
     { 
         mProgressBar.Fill();
         
         Debug.Log("Loading is completed");
+        
+        mStatus = eLoadingStatus.INACTIVE;
         
         // load meta game
     }
@@ -41,67 +78,121 @@ public class Loading : BaseLogic
         // включить заставку
         
         mStatus = eLoadingStatus.INITIAL_STATE;
-        mProgressBar = GameObject.Find("ProgressBar").GetComponent<ProgressBar>();
 
-        mTaskQueue = new TaskQueue(OnTerminated, OnCompleted);
-        mTaskQueue.AddTask("CheckGameVersion", CheckGameVersion);
-        mTaskQueue.AddTask("LoadManagers", LoadManagers);
-        mTaskQueue.AddTask("LoadGeneralObjects", LoadGeneralObjects);
-        mTaskQueue.AddTask("LoadConfigs", LoadConfigs);
-        mTaskQueue.AddTask("LoadProfile", LoadProfile);
+        mLoadingProgress = new Dictionary<eLoadingStatus, bool>();
+        mLoadingProgress.Add(eLoadingStatus.VERSION_CHECKED, false);
+        mLoadingProgress.Add(eLoadingStatus.MANAGERS_LOADED, false);
+        mLoadingProgress.Add(eLoadingStatus.GENERAL_LOADED, false);
+        mLoadingProgress.Add(eLoadingStatus.CONFIGS_LOADED, false);
+        mLoadingProgress.Add(eLoadingStatus.PROFILE_LOADED, false);
         
-        mProgressBar.Launch(mTaskQueue.NumberOfTasks(), IsTaskCompleted);
-        StartCoroutine("LoadingProcess");
+        mProgressBar = GameObject.Find("ProgressBar").GetComponent<ProgressBar>();
+        mProgressBar.Launch(mLoadingProgress.Count, IsTaskCompleted);
     }
     
-    IEnumerator LoadingProcess()
+    void Update()
     {
-        mTaskQueue.Launch();
-        yield return null;
+        switch (mStatus)
+        {
+            case eLoadingStatus.INITIAL_STATE:
+                StartCoroutine("CheckGameVersion");
+                break;
+
+            case eLoadingStatus.VERSION_CHECKED:
+                mLoadingProgress[eLoadingStatus.VERSION_CHECKED] = true;
+                StartCoroutine("LoadManagers");
+                break;
+
+            case eLoadingStatus.MANAGERS_LOADED:
+                mLoadingProgress[eLoadingStatus.MANAGERS_LOADED] = true;
+                StartCoroutine("LoadGeneralObjects");
+                break;
+
+            case eLoadingStatus.GENERAL_LOADED:
+                mLoadingProgress[eLoadingStatus.GENERAL_LOADED] = true;
+                StartCoroutine("LoadConfigs");
+                break;
+
+            case eLoadingStatus.CONFIGS_LOADED:
+                mLoadingProgress[eLoadingStatus.CONFIGS_LOADED] = true;
+                StartCoroutine("LoadProfile");
+                break;
+
+            case eLoadingStatus.PROFILE_LOADED:
+                mLoadingProgress[eLoadingStatus.PROFILE_LOADED] = true;
+                OnLoadingCompleted();
+                break;
+
+            case eLoadingStatus.TASK_FAILED:
+                OnLoadingTerminated();
+                break;
+        }
     }
 
     private bool IsTaskCompleted(int taskIndex)
     {
-        return mTaskQueue.IsTaskCompleted(taskIndex);
+        var result = false;
+
+        switch (taskIndex)
+        {
+            case 0:
+                result = mLoadingProgress[eLoadingStatus.VERSION_CHECKED];
+                break;
+            case 1:
+                result = mLoadingProgress[eLoadingStatus.MANAGERS_LOADED];
+                break;
+            case 2:
+                result = mLoadingProgress[eLoadingStatus.GENERAL_LOADED];
+                break;
+            case 3:
+                result = mLoadingProgress[eLoadingStatus.CONFIGS_LOADED];
+                break;
+            case 4:
+                result = mLoadingProgress[eLoadingStatus.PROFILE_LOADED];
+                break;
+        }
+
+        return result;
     }
 
-    private bool CheckGameVersion()
+    IEnumerator CheckGameVersion()
     {
         mStatus = eLoadingStatus.IN_PROGRESS;
         Debug.Log("Check game version");
         mStatus = eLoadingStatus.VERSION_CHECKED;
-        return true;
+        yield return null;
     }
     
-    private bool LoadManagers()
+    IEnumerator LoadManagers()
     {
         mStatus = eLoadingStatus.IN_PROGRESS;
         Debug.Log("Load managers");
         mStatus = eLoadingStatus.MANAGERS_LOADED;
-        return true;
+        yield return null;
     }
     
-    private bool LoadGeneralObjects()
+    IEnumerator LoadGeneralObjects()
     {
         mStatus = eLoadingStatus.IN_PROGRESS;
         Debug.Log("Load general objects");
         mStatus = eLoadingStatus.GENERAL_LOADED;
-        return true;
+        yield return null;
     }
     
-    private bool LoadConfigs()
+    IEnumerator LoadConfigs()
     {
         mStatus = eLoadingStatus.IN_PROGRESS;
         Debug.Log("Load configs");
         mStatus = eLoadingStatus.CONFIGS_LOADED;
-        return true;
+        yield return null;
     }
     
-    private bool LoadProfile()
+    IEnumerator LoadProfile()
     {
         mStatus = eLoadingStatus.IN_PROGRESS;
+        yield return new WaitForSeconds(2f);    //temp
         Debug.Log("Load profile (save + settings)");
         mStatus = eLoadingStatus.PROFILE_LOADED;
-        return true;
+        yield return null;
     }
 }
