6212000e5652cc16786dd15d1e399b1aa185872e
Alexey <kalinichenkoae@gmail.com>
2020-04-12 12:06:49 +0300

    Add ads scene logic

---
+++ Assets/Scripts/Game/Scenes/Ads/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private float mTargetTime;
+    private Text  mTimer;
+    
+    void Start()
+    {
+        mTimer = GameObject.Find("Timer").GetComponent<Text>();
+        
+        mTargetTime = 10;
+        mTimer.text = mTargetTime.ToString();
+    }
+    void Update()
+    {
+        mTargetTime -= Time.deltaTime;
+        mTimer.text = ((int)mTargetTime).ToString();
+
+        if (mTargetTime <= 0)
+        {
+            OnComplete();
+            mTimer.text = "0";
+        }
+    }
+
+    private void OnComplete()
+    {
+        // Exit
+    }
+}
